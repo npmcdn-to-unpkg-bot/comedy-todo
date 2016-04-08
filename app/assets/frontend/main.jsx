fdb591fd800efcb3d40ea5ee38a961cc48738f1e
Rene M. Paccha <paccha@gmail.com>
2016-04-07 19:53:34 -0500

    updates to main.jsx

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from 'react-bootstrap/lib/Button';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
+import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+
 class Main extends React.Component {
   render() {
     return(
-      <div>
-        <h1> I'm inside a DOM react component!</h1>
-    </div>
+      <div className="container">
+        <ButtonGroup>
+          <DropdownButton bsStyle="success" title="Dropdown for our App">
+            <MenuItem key="1">Vinigar</MenuItem>
+            <MenuItem key="2">Drink</MenuItem>
+          </DropdownButton>
+          <Button bsStyle="info">Keep</Button>
+          <Button bsStyle="info">Throw in Garbage</Button>
+        </ButtonGroup>
+      </div>
     )
   }
 }
